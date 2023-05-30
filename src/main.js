1f69dcb9647c817773813d23ccf948bfc72c0091
CRUX56 <designers_crux@live.com>
2023-05-30 10:42:23 -0400

    Added mailing.js to handle forms and emails on website.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
 
 import { BootstrapVue } from 'bootstrap-vue'
 import VueYoutube from 'vue-youtube'
@@ -8,6 +9,10 @@ import VueYoutube from 'vue-youtube'
 import Scrollspy from 'vue2-scrollspy';
 var VueScrollTo = require('vue-scrollto');
 
+import VueTinySlider from "vue-tiny-slider";
+var VueSlider = require('vue-tiny-slider');
+
+Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
 import "@/assets/scss/style.scss";
@@ -20,6 +25,7 @@ Vue.use(VueScrollTo)
 
 // Install BootstrapVue
 Vue.use(BootstrapVue)
+Vue.use(VueSlider)
 
 new Vue({
   router,
