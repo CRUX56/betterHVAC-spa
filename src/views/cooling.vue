d0c6cc644dc1f342753e39d5479ea23acb14d308
CRUX56 <designers_crux@live.com>
2023-06-11 14:09:13 -0400

    Updated Subpages with base template.

--- src/views/cooling.vue
+++ src/views/cooling.vue
@@ -13,8 +13,35 @@ export default {
     <div>
         <Navbar />
         <div>
-            <section class="hero-4-bg" :style="">
-
+            <section 
+              class="hero-4-bg" 
+              :style="{'background-image': 'url(' + require('@/assets/images/hero-4-bg.jpg') + ')'}" 
+              id="home">
+              <!--Header section-->
+              <div class="container">
+                <div class="row align-items-left">
+                    <div class="col-xl-12 col-lg-12">
+                        <h2 class="hero-4-title mb-4 text-shadow line-height-1_4">
+                            Page Title Or Service Title<br /> That Can Go Two Columns
+                        </h2>
+                    </div>
+                </div>
+              </div>
+            </section>
+            <section class="section" id="main-content">
+                <div class="container">
+                    <div class="row align-items-left">
+                        <div class="col-xl-12 col-lg-12">
+                            <h3>Cooling Title</h3>
+                            <p class="text-muted mb-4">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras feugiat nibh non viverra pulvinar. Nam lacinia, dolor at laoreet pellentesque, metus eros venenatis augue, eget tristique felis arcu et urna. Maecenas porttitor lobortis felis. Sed dictum et orci ut porttitor. Proin ultrices, leo sit amet consectetur tincidunt, nulla sapien elementum magna, quis venenatis mauris nulla eu leo. Nunc consequat sem libero, vel scelerisque orci dignissim a. Sed vitae justo sit amet urna semper semper. Pellentesque vestibulum finibus libero, sed varius justo gravida in. Vestibulum rhoncus at diam eget viverra.
+                            </p>
+                            <p class="text-muted mb-4">
+                                Etiam ut volutpat ex, vel faucibus eros. Sed nec turpis in dui hendrerit lacinia id pellentesque elit. Donec at ligula tristique enim bibendum iaculis eget a elit. Morbi faucibus venenatis tincidunt. Etiam ut semper libero. Praesent pellentesque luctus justo id tincidunt. Nulla eget mi tellus. Fusce faucibus, ante a accumsan condimentum, ligula turpis sollicitudin neque, vitae consequat odio mauris ut tortor. Quisque ante lectus, congue cursus felis quis, auctor eleifend eros. Nulla laoreet ante eget congue scelerisque. Vivamus id mauris vel nibh volutpat ornare vestibulum a odio. Suspendisse lacinia leo nec ultrices interdum. Cras vitae elit sed nibh semper pulvinar.
+                            </p>
+                        </div>
+                    </div>
+                </div>
             </section>
         </div>
         <Testimonials />
